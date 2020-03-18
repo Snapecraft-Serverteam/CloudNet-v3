5671efd8593fe1c40ea601a97d94aef5038da8e2
Ayley <larshatzky@gmail.com>
2020-03-19 00:25:51 +0100

    Add Permission Sponge Support. (I hope it so much!)

--- cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/SpongeCloudNetCloudPermissionsPlugin.java
+++ cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/SpongeCloudNetCloudPermissionsPlugin.java
@@ -11,9 +11,6 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-import java.lang.reflect.Field;
-import java.util.concurrent.ExecutionException;
-
 @Plugin(
         id = "cloudnet_cloudperms",
         name = "CloudNet-CloudPerms",
