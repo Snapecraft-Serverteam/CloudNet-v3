7a5a40fa34c3dc4eeb07f5af369160ae0acde42b
Ayley <larshatzky@gmail.com>
2020-03-19 00:05:01 +0100

    Add Permission Sponge Support

--- cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/SpongeCloudNetCloudPermissionsPlugin.java
+++ cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/SpongeCloudNetCloudPermissionsPlugin.java
@@ -1,11 +1,16 @@
 package de.dytanic.cloudnet.ext.cloudperms.sponge;
 
+import de.dytanic.cloudnet.common.Validate;
 import de.dytanic.cloudnet.driver.CloudNetDriver;
 import de.dytanic.cloudnet.wrapper.Wrapper;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.ExecutionException;
 
 @Plugin(
         id = "cloudnet_cloudperms",
@@ -16,6 +21,13 @@ import org.spongepowered.api.plugin.Plugin;
 )
 public final class SpongeCloudNetCloudPermissionsPlugin {
 
+    private SpongeCloudNetCloudPermissionsPlugin instance;
+
+    @Listener
+    public void onStart(){
+        instance = this;
+    }
+
     @Listener
     public void onEnable(GameStartedServerEvent event) {
 
@@ -25,5 +37,9 @@ public final class SpongeCloudNetCloudPermissionsPlugin {
     public void onDisable(GameStoppingServerEvent event) {
         CloudNetDriver.getInstance().getEventManager().unregisterListeners(this.getClass().getClassLoader());
         Wrapper.getInstance().unregisterPacketListenersByClassLoader(this.getClass().getClassLoader());
+    }
+
+    public SpongeCloudNetCloudPermissionsPlugin getInstance() {
+        return instance;
     }
 }
---
+++ cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/listener/SpongeCloudNetCloudPermissionsPlayerListener.java
@@ -0,0 +1,28 @@
+package de.dytanic.cloudnet.ext.cloudperms.sponge.listener;
+
+import de.dytanic.cloudnet.ext.cloudperms.CloudPermissionsHelper;
+import de.dytanic.cloudnet.ext.cloudperms.CloudPermissionsManagement;
+import de.dytanic.cloudnet.ext.cloudperms.sponge.SpongeCloudNetCloudPermissionsPlugin;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+
+public class SpongeCloudNetCloudPermissionsPlayerListener {
+    private SpongeCloudNetCloudPermissionsPlugin plugin;
+
+    public SpongeCloudNetCloudPermissionsPlayerListener(SpongeCloudNetCloudPermissionsPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    @Listener
+    public void onJoin(final ClientConnectionEvent.Login event){
+        CloudPermissionsHelper.initPermissionUser(event.getProfile().getUniqueId(), event.getProfile().getName().get(),true);
+
+        Sponge.getEventManager().registerListeners(this,new SpongeCloudNetCloudPermissionsPlayerListener(plugin));
+    }
+
+    @Listener
+    public void onQuit(final ClientConnectionEvent.Disconnect event){
+        CloudPermissionsManagement.getInstance().getCachedPermissionUsers().remove(event.getTargetEntity().getUniqueId());
+    }
+}
