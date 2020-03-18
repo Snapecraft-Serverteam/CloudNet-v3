dba9b022067121d9eaa788b8b8fd071e3b13adc9
Ayley <larshatzky@gmail.com>
2020-03-19 00:13:18 +0100

    Add Permission Sponge Support. (I hope it so much!!!)

--- cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/listener/SpongeCloudNetCloudPermissionsPlayerListener.java
+++ cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/sponge/listener/SpongeCloudNetCloudPermissionsPlayerListener.java
@@ -16,7 +16,7 @@ public class SpongeCloudNetCloudPermissionsPlayerListener {
 
     @Listener
     public void onJoin(final ClientConnectionEvent.Login event){
-        CloudPermissionsHelper.initPermissionUser(event.getProfile().getUniqueId(), event.getProfile().getName().get(),true);
+        CloudPermissionsHelper.initPermissionUser(event.getProfile().getUniqueId(), event.getProfile().getName().get(),false);
 
         Sponge.getEventManager().registerListeners(this,new SpongeCloudNetCloudPermissionsPlayerListener(plugin));
     }
